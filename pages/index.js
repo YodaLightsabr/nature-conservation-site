d59b73221954c5325fc8089744e312e5eab09055
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-05-02 10:18:06 -0700

    Changes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
-export default function Home () {
+export default function Home() {
   return (
     <div style={{
-      height: '100vh',
     }}>
-      <main style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}>
-        <h1>Nature Conservation Site</h1>
-      </main>
+      <h1>Nature Conservation</h1>
+
+      <h2>Why are native plants important?
+        <p>Native California plants make up a large part of the state’s beauty and environmental community. They promote pollinators and other wildlife, so they are important for an ecosystem to thrive. These plants are the groundwork for life and keep our community uplifted. With a recent increase in urbanization and invasive plant species, the native plant population has declined drastically, leading to effects on important ecosystems, wildlife, and biodiversity.</p>
+
+        <h2>What can you do to help?</h2>
+        <p>As a community, it is important that we all take action against this issue. It is as simple as building a garden in your backyard much like the garden in front of you. This native wildflower garden was created with the help of volunteers in just a manner of weeks, and involved few materials. Local nurseries like the Tree of Life nursery, Devil Mountain Wholesale Nursery, Roger’s garden, and the Environmental Nature Center all have native plants that are available to be purchased and grown.</p>
+      </h2>
     </div>
   )
 }
